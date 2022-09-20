dfd4f74ffb4e09586c1ca17a0b2ed9eb484b724f
Ivan Volyk <volyk.ivan@gmail.com>
2022-09-20 22:17:48 +0300

    Solution exercise 1.1

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Решение задач по книге “1 400 задач по программированию” Д. Златопольский</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style/style.css"/>
+</head>
+<body>
+	<div class="container">
+		<h1>Решение задач по книге “1 400 задач по программированию” Д. Златопольский</h1>
+		<h2>Глава 1. Вывод информации на экран</h2>
+		<div class="block">
+			<h3>1.1. Вывести одной строкой чисел 9, 23 и 14 с одним пробелом между ними. Текст '9 23 14' использовать запрещено.</h3>
+			<?php
+
+				$a = 9;
+				$b = 23;
+				$c = 14;
+
+			?>
+
+			<p><?php echo $a . ' ' . $b . ' ' . $c;?></p>
+		</div>
+	</div>
+
+	
+
+
+
+
+</body>
+</html>
+
+
+
+
