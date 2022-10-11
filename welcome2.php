9ad27e3b699343c8abd57d4637b09265e1dc42aa
Ivan Volyk <volyk.ivan@gmail.com>
2022-10-11 17:59:04 +0300

    Solution exercise 1.13

--- welcome2.php
+++ welcome2.php
@@ -1,7 +1,17 @@
 <?php 
 
 $var = implode('', $_GET);
+$var_name = implode('', $_POST);
+$i = 1;
+$plus = $var_name + $i;
+$minus = $var_name - $i;
 
-echo $var;
+if ( $var == true ) {
+	echo $var;
+} else {
+	echo 'Вы ввели число –&nbsp;' . $var_name . '.<br>';
+	echo 'Следующее за числом&nbsp;' . $var_name . '&nbsp;число –&nbsp;' . $plus . '.<br>';
+	echo 'Для числа&nbsp;' . $var_name . '&nbsp;предыдущее число –&nbsp;' . $minus . '.';
+}
 
 ?> 
